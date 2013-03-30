233e30ea76dbf91c22f2b66c1ae2565ec6f61671
Paul English <paul@onfrst.com>
2013-03-30 14:23:05 -0600

    Remove unneeded comments

--- src/main/java/database/InvalidModelException.java
+++ src/main/java/database/InvalidModelException.java
@@ -11,32 +11,27 @@ public class InvalidModelException extends Exception
 	public InvalidModelException()
 	{
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidModelException(String message)
 	{
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidModelException(String message, Throwable cause)
 	{
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidModelException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace)
 	{
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 	public InvalidModelException(Throwable cause)
 	{
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 }
