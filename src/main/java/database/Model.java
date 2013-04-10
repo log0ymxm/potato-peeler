4609d595763c6de637c6b3e16e021c68efda2e66
Paul English <paul@onfrst.com>
2013-04-10 17:35:14 -0600

    Add some documentation

--- src/main/java/database/Model.java
+++ src/main/java/database/Model.java
@@ -1,17 +1,27 @@
 package database;
 
-
 public abstract class Model
 {
 
 	protected boolean dirty = false;
 	protected boolean fresh = false;
 
+	/**
+	 * This means that a models properties have been changed, but we haven't
+	 * saved this model since changing them.
+	 * 
+	 * @return
+	 */
 	public final boolean isDirty()
 	{
 		return this.dirty;
 	}
 
+	/**
+	 * This means a model is brand new and no database record exists.
+	 * 
+	 * @return
+	 */
 	public final boolean isFresh()
 	{
 		return this.fresh;
