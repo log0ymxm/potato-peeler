bf6a26172185800d9715adbe9c5757824303fc64
Paul English <paul@onfrst.com>
2013-04-29 13:53:26 -0600

    Fix users table migration

--- src/main/db/migrations/20130425120800.sql
+++ src/main/db/migrations/20130425120800.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `userinfo` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `user` varchar(45) NOT NULL,
-  `pass` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
   PRIMARY KEY  (`id`)
 );
-  
+
 INSERT INTO `users` (`id`,`username`,`password`) VALUES (1,'admin','password');
