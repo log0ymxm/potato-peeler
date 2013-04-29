52ca68d43dba3b2d7d6161a22ba3920c1a4f02ff
Paul English <paul@onfrst.com>
2013-04-29 13:54:40 -0600

    Implement Transcript.findById()

--- src/main/java/models/Transcript.java
+++ src/main/java/models/Transcript.java
@@ -1,8 +1,13 @@
 package models;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
+import database.DBFactory;
 import database.Model;
 
 public class Transcript extends Model
@@ -13,15 +18,18 @@ public class Transcript extends Model
 	private String id;
 	private boolean isPredicted;
 
+	public Transcript(ResultSet resultSet) throws SQLException
+	{
+		this(resultSet.getString("id"), resultSet.getDate("date"), resultSet
+				.getBoolean("isPredicted"), false, false);
+	}
+
 	protected Transcript(String id, Date date, Boolean isPredicted,
 			Boolean dirty, Boolean fresh)
 	{
 		super("transcripts", new ArrayList<String>()
 		{
 
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = -7904643137101173617L;
 
 			{
@@ -32,9 +40,6 @@ public class Transcript extends Model
 		}, new ArrayList<String>()
 		{
 
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = 6517481081926596080L;
 
 			{
@@ -42,6 +47,48 @@ public class Transcript extends Model
 		}, dirty, fresh);
 		// TODO Auto-generated constructor stub
 		throw new UnsupportedOperationException();
+	}
+
+	public static Transcript findById(String id)
+	{
+		Transcript transcript = null;
+
+		Connection connection = DBFactory.getConnection();
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+
+		try
+		{
+
+			String query = "SELECT id, is_predicted, date FROM transcripts WHERE id = ? LIMIT 1";
+			statement = connection.prepareStatement(query);
+			statement.setString(1, id);
+			resultSet = statement.executeQuery();
+
+			while (resultSet.next())
+			{
+				transcript = new Transcript(resultSet);
+			}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+			System.exit(1);
+		}
+		finally
+		{
+			try
+			{
+				resultSet.close();
+				statement.close();
+				DBFactory.closeConnection(connection);
+			}
+			catch (Exception exception)
+			{
+				exception.printStackTrace();
+			}
+		}
+		return transcript;
 	}
 
 	public Date getDate()
